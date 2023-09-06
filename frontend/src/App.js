de9ac378f1992b11c76daea96aa06cde20fb72c5
Uzair Hameed <uzairhameed98@gmail.com>
2023-09-06 19:02:16 +0500

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <h1>Hello WOrld</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container></Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
